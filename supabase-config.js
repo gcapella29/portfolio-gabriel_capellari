8a42239a0639ef11bb6a12231f2120f987ee6779
gcapella29 <gcapellari1@gmail.com>
2026-08-12 15:57:29 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.VITRINE_SUPABASE = Object.freeze({
+  url: "https://ownoyzpjiqbzgaeaoyzl.supabase.co",
+  publishableKey: "sb_publishable_q1UwNCcinl7S6KN-oCU1rA_99_17BXw",
+  projectSlug: "gabriel-capellari"
+});
